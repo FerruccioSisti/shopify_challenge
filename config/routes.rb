1400fccd37f0525d4788f7024af5aa4ddf4390c2
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-05-12 19:33:50 -0400

    Added different name for user_posts page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   get 'about', to: 'pages#about'
+  get 'your_posts', to: 'posts#user_posts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
